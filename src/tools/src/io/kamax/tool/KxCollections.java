386a7a13f51c0d7bdadd959fa44051b843818b35
Maxime Dor <max@kamax.io>
2015-08-16 20:07:25 +0200

    Easy collections sorter

---
+++ src/tools/src/io/kamax/tool/KxCollections.java
@@ -0,0 +1,36 @@
+/*
+ * Hyperbox - Virtual Infrastructure Manager
+ * Copyright (C) 2015 - Maxime Dor
+ * 
+ * http://kamax.io/hbox/
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package io.kamax.tool;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+
+public class KxCollections {
+
+   public static <T> List<T> sort(List<T> list, Comparator<T> comp) {
+      Collections.sort(list, comp);
+      return list;
+   }
+
+}
